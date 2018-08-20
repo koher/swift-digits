504b54d9afed7c3a5ce37264a602e570aa8bdf8c
Yuta Koshizawa <koher@koherent.org>
2018-08-21 01:46:39 +0900

    Improve importing in main.swift

--- Models/main.swift
+++ Models/main.swift
@@ -1,18 +1,18 @@
 import Python
 
-let datasets = Python.import("sklearn.datasets")
-let svm = Python.import("sklearn.svm")
-let model_selection = Python.import("sklearn.model_selection")
+let load_digits = Python.import("sklearn.datasets").load_digits
+let LinearSVC = Python.import("sklearn.svm").LinearSVC
+let train_test_split = Python.import("sklearn.model_selection").train_test_split
 
-let classifier = svm.LinearSVC()
-let dataset = datasets.load_digits()
-let (X_train, X_test, y_train, y_test) = model_selection.train_test_split(dataset["data"], dataset["target"]).tuple4
+let classifier = LinearSVC()
+let dataset = load_digits()
+let (X_train, X_test, y_train, y_test)
+    = train_test_split(dataset["data"], dataset["target"]).tuple4
 classifier.fit(X_train, y_train)
 
 print("train: \(classifier.score(X_train, y_train))")
 print("test:  \(classifier.score(X_test, y_test))")
 
 let coremltools = Python.import("coremltools")
-
 let coreml_model = coremltools.converters.sklearn.convert(classifier)
 coreml_model.save("Digits.mlmodel")
